b98f3bd70a80092697cf3c67e350c752c0924769
Daniil <daniilkabko@gmail.com>
2022-08-24 21:34:38 +0300

    Fix component StopWatch with setInterval at setTimeout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Chao from "./components/ChaoSection/Chao";
+// import Calendar from "./components/Calendar";
+import StopWatch from './components/StopWatch/index';
+
+const App = () => {
+  return (
+    <>
+      <StopWatch />
+      {/* <Calendar /> */}
+    </>
+  );
+};
+export default App;
